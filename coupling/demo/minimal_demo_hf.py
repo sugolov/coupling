6523ccdb64bd43d63e144733d408c012c74d766a
Anton <49133080+sugolov@users.noreply.github.com>
2025-04-06 18:59:18 -0400

    code release

---
+++ coupling/demo/minimal_demo_hf.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+from main import run_coupling_hf
+
+def run(model_path="meta-llama/Meta-Llama-3-8B"):
+    model_name = os.path.normpath(os.path.basename(model_path))
+
+    bnb_config = BitsAndBytesConfig(load_in_4bit=True)
+
+    model = AutoModelForCausalLM.from_pretrained(
+                model_path,
+                device_map="cuda",
+                trust_remote_code=True,
+                quantization_config=bnb_config
+            )
+
+    tokenizer = AutoTokenizer.from_pretrained(
+            model_path,
+            use_fast=True,
+        )
+
+    prompts = ["What is the capital of France? The capital is"]
+
+    out = run_coupling_hf(model, tokenizer, model_name, prompts, save=True, verbose=False)
+
+    return out
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model-path", action="store", type=str)
+    args = parser.parse_args()
+    run(**vars(args))
